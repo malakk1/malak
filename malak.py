2368bfdc0c829d2923261d417d613d7aa72bb59a
malakk1 <105747495+malakk1@users.noreply.github.com>
2022-05-17 22:30:23 +0300

    add

--- malak.py
+++ malak.py
@@ -16,7 +16,6 @@ plt.show()
 
 
 
-
 # You may be wondering why the x-axis ranges from 0-3 and the y-axis from 1-4. If you provide a single list or array to plot, matplotlib assumes it is a sequence of y values, and automatically generates the x values for you. Since python ranges start with 0, the default x vector has the same length as y but starts with 0. Hence the x data are [0, 1, 2, 3].
 # 
 # plot is a versatile function, and will take an arbitrary number of arguments. For example, to plot x versus y, you can write:
